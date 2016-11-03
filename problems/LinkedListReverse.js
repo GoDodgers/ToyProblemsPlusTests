cd779284b676ae7cb2f45d4c869bf5d7529c550b
Alberto Esquivias <alberto.m.esquivias@gmail.com>
2016-11-02 17:11:04 -0700

    [problem] adds LinkedListReverse solution

---
+++ problems/LinkedListReverse.js
@@ -0,0 +1,35 @@
+/*
+
+Linked List Reverse
+Write a function that reverses a linked list.
+
+Explanation:
+
+Given the below linked list:
+
+A -> B -> C -> D -> E -> null
+
+Return:
+
+E -> D -> C -> B -> A -> null
+
+‘Constraint 1: Do this in linear time’
+
+‘Constraint 2: Do this in constant space’
+
+‘Constraint 3: Do not mutate the original nodes by adding any new properties’
+
+*/
+
+var reverseLinkedList = function(linkedList) {
+	let current = linkedList;
+	let previous = null;
+
+	while(current) {
+		let save = current.next;
+		current.next = previous;
+		previous = current;
+		current = save;
+	}
+	return previous;
+};
